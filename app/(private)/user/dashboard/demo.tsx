b3a1825208dae7476fd0c3473c70843ed24ae51d
prashanth <prashanthgoud7675@gmail.com>
2025-07-15 15:41:55 -0500

    Authentication

---
+++ app/(private)/user/dashboard/demo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function demo() {
+  return (
+    <div>
+      <h2>rehbfhcjjfchrefhcfjdhnkfhgruhjdcjhfgehfgjhufguedhhgf</h2>
+    </div>
+  )
+}
+
+export default demo
